4b9c3d2c812283ff7b87fa7bb10b344e941bde4a
Raquel Mendes <raquelmqs11@gmail.com>
2024-08-21 17:44:19 -0300

    Create exercício 03.py

---
+++ exercícios lógica 2/exercício 03.py
@@ -0,0 +1,10 @@
+a = 4
+b = 2
+soma = a + b 
+produto = a * b 
+subtração = a - b 
+divisão = a / b
+print("Soma:", soma)
+print("Produto:", produto)
+print("Subtração:", subtração)
+print("Divisão:", divisão)

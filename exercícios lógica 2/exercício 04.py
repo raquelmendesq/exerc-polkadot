e5ec920f845ecbef69a63bdb2d739d82332a574f
Raquel Mendes <raquelmqs11@gmail.com>
2024-08-21 18:04:32 -0300

    Create exercício 04.py

---
+++ exercícios lógica 2/exercício 04.py
@@ -0,0 +1,11 @@
+temperatura=int(input("Qual a temperatura da sua cidade?"))
+
+if temperatura >30: 
+      print("Está quente")
+
+elif temperatura <15:
+    print("Está frio")
+
+else:
+
+    print("Está agradável")

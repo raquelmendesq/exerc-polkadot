bcc6dac9bcacc02b09857e8774e296485a19d188
Raquel Mendes <raquelmqs11@gmail.com>
2024-08-22 20:17:23 -0300

    Create exercício 09.py

---
+++ exercícios lógica 2/exercício 09.py
@@ -0,0 +1,5 @@
+frase = input("Digite uma frase:")
+letra = input("Digite uma letra:")
+contagem= frase.count(letra)
+print("A letra:", letra, "aparece", contagem, "vezes na frase digitada" )
+     
